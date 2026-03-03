4c6b285f9b8ef480bbd364edced95cca3cc8a28c
Quan <216721166+NguyenHongQuan2k6@users.noreply.github.com>
2026-03-03 12:00:56 +0700

    Update table_nhaccuatui.py

--- trending music in data science/table_nhaccuatui.py
+++ trending music in data science/table_nhaccuatui.py
@@ -1,12 +1,15 @@
 import pandas as pd
 import mainpage_nhaccuatui as mp
 
-# 1. Bế thẳng danh sách dữ liệu từ mainpage vào Pandas
+# Bế thẳng danh sách dữ liệu từ mainpage vào Pandas
 df = pd.DataFrame(mp.list_top_trending)
 
-# 2. Sắp xếp lại thứ tự cột cho đẹp (bỏ qua bước read_csv)
+# Xử lý dữ liệu trùng
+df = df.drop_duplicates(subset=["Song name", "Artist"])
+
+# Sắp xếp lại thứ tự cột cho đẹp (bỏ qua bước read_csv)
 df = df[["Time", "Date Updated", "Song name", "Song rank", "Artist", "Hearts (Real)", "Shares (Real)"]]
 
-# 3. In ra màn hình
+# In ra màn hình => Kiểm tra tổng quan dữ liệu
 print(f"\nFound the top {len(mp.list_top_trending)} trending Vpop songs\n")
 print(df.to_markdown(index=False))
